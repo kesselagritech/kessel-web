e575008612e4ad07f292ce992db73a71a5d8a79f
ATINE MVOM PHILIPPE ANDRE <contact@kesselagritech.com>
2026-07-07 14:30:06 +0100

    slug manioc: cohérence convention

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/bibliotheque/business-plan-manioc',
+        destination: '/bibliotheque/manioc',
+        permanent: true, // 301
+      },
+    ];
+  },
 };
 
 export default nextConfig;
